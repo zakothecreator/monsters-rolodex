943fc4242e9af4c87a91676bf1c06360cb73a151
Zakothecreator <zakoaziz1@gmail.com>
2020-10-27 19:12:11 +0100

    Updates

---
+++ precache-manifest.a82ea39537ef98a27da12a9d69d31690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17dbaa378467a8290bca2350a684f554",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dd113f79b0ca27883bd9",
+    "url": "/monsters-rolodex/static/css/main.68ce74a8.chunk.css"
+  },
+  {
+    "revision": "2d19631dba197ad7891e",
+    "url": "/monsters-rolodex/static/js/2.e1627e68.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.e1627e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd113f79b0ca27883bd9",
+    "url": "/monsters-rolodex/static/js/main.a6e9508b.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
